c8716855f69ac8d17baf9828e7047f993a4e5836
TristanCarnesciali <tristan.carnesciali@gmail.com>
2021-09-29 16:29:21 +0200

    <feature>(presentation) ajout sql pour afficher les personne

--- Comeleon/migrations/Version20210929131545.php
+++ Comeleon/migrations/Version20210929131545.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210924131320 extends AbstractMigration
+final class Version20210929131545 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,11 +21,13 @@ final class Version20210924131320 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE avis (id INT AUTO_INCREMENT NOT NULL, id_user INT NOT NULL, commentaire LONGTEXT DEFAULT NULL, titre VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE presentation (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, age INT NOT NULL, present_activite VARCHAR(800) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE avis');
+        $this->addSql('DROP TABLE presentation');
     }
 }
--- Comeleon/src/Controller/PresentationController.php
+++ Comeleon/src/Controller/PresentationController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Presentation;
 
 class PresentationController extends AbstractController
 {
@@ -13,8 +14,11 @@ class PresentationController extends AbstractController
      */
     public function index(): Response
     {
+        $repo = $this->getDoctrine()->getRepository(Presentation::class);
+        $presentation = $repo->findALL();
         return $this->render('presentation/index.html.twig', [
             'controller_name' => 'PresentationController',
+            'presentations' => $presentation,
         ]);
     }
 }
